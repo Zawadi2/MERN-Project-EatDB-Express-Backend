b0a9058b79f6b2221d7708d30c6881e6918ea7f9
Zawadi2 <zawadin95@gmail.comm>
2024-08-14 14:17:43 -0500

    backend

--- models/food.js
+++ models/food.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 
 const foodSchema = new mongoose.Schema({
-   name: {
-    type:String,
+  name: { type: String, required: true },
+  ingredients: String,
+  isVegan: String,
+  rating: {
+    type: Number,
     required: true,
-   },
-   ingredients: String,
-   isVegan: String,
-   rating:Number,
-   price:String
-});  
+    min: [1, "Rating must be at least 1"],
+    max: [5, "Rating must be at most 5"],
+  },
+  price: String,
+});
 
-module.exports = foodSchema
+module.exports = foodSchema;
